5467b73e05118eb45ee59d5a61d6d8a30e359ec9
Andy Bradford <andycb@server.fake>
2021-02-06 21:25:30 +0000

    Add error logging to file

--- src/electronApp/angularMain.ts
+++ src/electronApp/angularMain.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './angularApp/app.module';
 import { environment } from './environments/environment';
+import { ErrorLogger } from './core/errorLogger'
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +11,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+ErrorLogger.SetUp();
