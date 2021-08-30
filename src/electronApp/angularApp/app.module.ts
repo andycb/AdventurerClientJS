a96303bd0fdec754003db4909ea73fc0364cbc17
Andy Kitchen <kitchen.andy+git@gmail.com>
2021-08-30 17:22:23 +1000

    fixed building on Linux
    The main problem was that by default the
    Mac OS X filesystem is case insensitive.
    There was a import for 'App' instead of 'app'
    A build error is revealed on linux where the filesystem
    is case sensisitve.
    
    The electron build now works, tested on Ubuntu Linux 20.04

--- src/electronApp/angularApp/app.module.ts
+++ src/electronApp/angularApp/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './components/App/app.component';
+import { AppComponent } from './components/app/app.component';
 import { ConnectFormComponent } from './components/connect-form/connect-form.component';
 import { RoutingModule } from './routing/routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +12,7 @@ import { AboutComponent } from './components/about/about.component';
 import { StatusComponent } from './components/status/status.component';
 import { DebugComponent } from './components/debug/debug.component';
 import { PrintComponent } from './components/print/print.component';
-import { DragDropDirective } from './ directives/dragDropDirective';
+import { DragDropDirective } from './directives/dragDropDirective';
 import { CameraComponent } from './components/camera/camera.component';
 import { ConnectionErrorDialogComponent } from './components/connection-error-dialog/connection-error-dialog.component';
 import { StopPrintingConfirmationDialogComponent } from './components/stop-printing-confirmation-dialog/stop-printing-confirmation-dialog.component';
